01ead2152cd74da5cd62b9bd4cfdc2da5ae0f8b8
kumares93 <40257292+kumares93@users.noreply.github.com>
2018-07-25 12:10:58 +0530

    Add files via upload

---
+++ js/jsrc.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    
+ $(".item1").click(function(){
+    
+        var x=$('.item1 ul').css('display');
+        if(x=='block'){
+        $(".item1 ul").css('display','none');
+        }else
+        $(".item1 ul").css('display','block');
+
+        $(".item1 ul li ul,.item2 ul,.item3 ul,.item4 ul,.item5 ul").css('display','none');
+});
+$(".item1 ul li").mouseover(function(){
+        $(".item1 ul li ul").css('display','block');
+});
+$(".item1 ul li").mouseout(function(){
+        $(".item1 ul li ul").css('display','none');
+});
+$(".item2").click(function(){
+    
+        var x=$('.item2 ul').css('display');
+        if(x=='block'){
+        $(".item2 ul").css('display','none');
+        }else
+        $(".item2 ul").css('display','block');
+        $(".item1 ul,.item3 ul,.item4 ul,.item5 ul").css('display','none');
+});
+$(".item3").click(function(){
+    
+        var x=$('.item3 ul').css('display');
+        if(x=='block'){
+        $(".item3 ul").css('display','none');
+        }else
+        $(".item3 ul").css('display','block');
+        $(".item2 ul,.item1 ul,.item4 ul,.item5 ul").css('display','none');
+});
+$(".item4").click(function(){
+    
+        var x=$('.item4 ul').css('display');
+        if(x=='block'){
+        $(".item4 ul").css('display','none');
+        }else
+        $(".item4 ul").css('display','block');
+        $(".item2 ul,.item3 ul,.item1 ul,.item5 ul").css('display','none');
+});
+$(".item5").click(function(){
+    
+        var x=$('.item5 ul').css('display');
+        if(x=='block'){
+        $(".item5 ul").css('display','none');
+        }else
+        $(".item5 ul").css('display','block');
+        $(".item2 ul,.item3 ul,.item4 ul,.item1 ul").css('display','none');
+});
+//$('*').click(function () {
+  //      $(".item2 ul,.item3 ul,.item4 ul,.item1 ul,.item5 ul").css('display','none');
+//})
+});
