39cb4d39f87d6bb7adf187daf3ec3bbc722d4810
thanakumares <kumaresntk@gmail.com>
2018-07-26 11:30:44 +0530

    updated

--- js/jsrc.js
+++ js/jsrc.js
@@ -56,3 +56,44 @@ $(".item5").click(function(){
   //      $(".item2 ul,.item3 ul,.item4 ul,.item1 ul,.item5 ul").css('display','none');
 //})
 });
+
+$(document).ready(function(){
+        $('.your-class').slick({
+          dots:true,
+          autoplay:true,
+          arrows:false
+        });
+        $('.slide-div').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed:1000
+
+      });
+      $('.slide-div2').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed:1000
+
+      });
+  });
+  
+  
+  
+  
+  
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+  } else {
+      document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}   
